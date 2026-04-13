b5d58dca6509c3fc1f7b9f7f2141bf5db423b228
aarf101 <ayoub.arfaoui@supcom.tn>
2026-04-13 18:51:51 +0100

    chore: remove temporary test_synthesizer.py script

--- test_synthesizer.py
+++
@@ -1,9 +0,0 @@
-import re
-from datetime import datetime
-
-def clean_text(text):
-    if not text: return ''
-    text = re.sub(r'<[^>]+>', '', text)
-    text = text.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&nbsp;', ' ').replace('&#39;', "'").replace('&quot;', '"')
-    text = ' '.join(text.split())
-    return text[:200]
